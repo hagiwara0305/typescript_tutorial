3c7232dc2bedb45413e8fa992ddd646e93f0e14c
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-01-26 21:25:22 +0900

    VSCode側びバグがあるらしい... tsc --outDir ./dir/ .ts　で実行した...

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,11 @@
-function greeter(person) {
-    return "Hello, " + person;
+// 変数に型を与えて宣言。
+var onigiri: string = 'onigiri', // String型
+    calories: number = 200;  // Number型
+
+// 関数の仮引数にも受け入れる型を定義。
+// {}の前にあるstringは関数の返す値の型を定義している。
+function todaysMeal(food: string, energy: number): string {
+  return `My ${food} has ${energy} calories`
 }
 
-let user = "Jane User";
-
-document.body.innerHTML = greeter(user);
+todaysMeal(onigiri, calories)
